5e821b4a8437b9df8bf881905ea50d7379a15f3b
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2025-03-27 17:52:32 +0200

    Update fn-string.js

--- fn-string.js
+++ fn-string.js
@@ -1,4 +1,8 @@
 function isEmpty(str) {
-    // Returns true if string is empty, null, undefined, or whitespace-only
-    return !str || str.trim() === "";
-  }
+  // Returns true if string is empty, null, undefined, or whitespace-only
+  return !str || str.trim() === "";
+}
+
+function checkSubstring(mainString, subString) {
+  return mainString.includes(subString);
+}
